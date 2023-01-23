ac0ad2920ab930a414f81ef3543acd5889e10e27
sliverKi <121347506+sliverKi@users.noreply.github.com>
2023-01-23 20:39:03 +0900

    Update serializers.py
    ADD version3 
    Version3 is use ModelSerialize

--- ver.2/categories/serializers.py
+++ ver.2/categories/serializers.py
@@ -5,8 +5,17 @@ from .models import Category
 #  Category가 어떻게 API바깥으로 나갈때 표시되는 방법을 정할 수 있다.
 # 1, 어떻게 번역할것인지
 # 2, 무엇을 번역할것인지 :: 무엇을 보여주고 무엇을 안보여줄지
+#-----------------------vERSION3--------------------------------#
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        # serializer가 나의 category model을 위한 serializer를 만들고
+        # 추가로 create-Method, update-Method를 만들어줌.
 
-
+        # fields = ("name","kind","created_at",)  # want show fields one by one
+        # exclude = ("created_at",)  # exclude this fields
+        fields = "__all__"  # want show all fields from category model
+#-------------------------VERSION2------------------------------#        
 class CategorySerializer(serializers.Serializer):
     # DB에 있는 객체들은 모두 PK를 가지고 있다.
     # pk를 이용하여 커스터 마이징을 할 수도 있다.
